7ce76ba675fe61a42a229f08dbea76b42d21ef87
Manoj Gaonkar <manojgaonkar79@gmail.com>
2026-03-13 23:36:37 +0530

    Update in the config file

--- common/config.py
+++ common/config.py
@@ -2,11 +2,7 @@
 
 from pathlib import Path
 
-<<<<<<< HEAD
-SERVER_HOST = "192.168.1.240"
-=======
 SERVER_HOST = "0.0.0.0"
->>>>>>> db16acf1ed0c7e09e7c06c2dfc2307b6ad8d531f
 SERVER_PORT = 9999
 BUFFER_SIZE = 4096
 
@@ -20,8 +16,4 @@ PACKET_LOSS_THRESHOLD_PERCENT = 10.0
 LOG_DIRECTORY = Path(__file__).resolve().parents[1] / "logs"
 EVENT_LOG_FILE = LOG_DIRECTORY / "event_log.txt"
 
-<<<<<<< HEAD
 PACKET_SEPARATOR = "|"
-=======
-PACKET_SEPARATOR = "|"
->>>>>>> db16acf1ed0c7e09e7c06c2dfc2307b6ad8d531f
